fa64520fcca30ae2c6fe49a14a4f1e4f8a5dcc87
senaygui <senaygeletaw@gmail.com>
2016-08-31 05:05:50 +0300

    created post migration, model, vaildation and relationships

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160831010941) do
+ActiveRecord::Schema.define(version: 20160831012522) do
+
+  create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.text     "content",    limit: 65535
+    t.integer  "user_id"
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.index ["user_id", "created_at"], name: "index_posts_on_user_id_and_created_at", using: :btree
+    t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                  default: "", null: false
@@ -31,4 +40,5 @@ ActiveRecord::Schema.define(version: 20160831010941) do
     t.index ["username"], name: "index_users_on_username", unique: true, using: :btree
   end
 
+  add_foreign_key "posts", "users"
 end
